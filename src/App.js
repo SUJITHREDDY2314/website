f1f5bace472b40554f81b5dd2b654e778fb0c0fe
SUJITHREDDY2314 <mereddysujithreddy@gmail.com>
2025-01-24 22:39:14 -0600

    Initialize React project with basic landing page for Maha Naga Kali LLC

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="welcome-content">
+        <h1>Welcome to</h1>
+        <h2 className="company-name">Maha Naga Kali LLC</h2>
+        <div className="decorative-line"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
